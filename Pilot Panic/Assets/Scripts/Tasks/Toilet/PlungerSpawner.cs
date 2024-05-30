4ea2acee29b6f768df3318684840a3b4fdf54091
jajara1 <146236551+jajara1@users.noreply.github.com>
2024-05-30 11:22:07 -0700

    added plunger(no functionality yet)

---
+++ Pilot Panic/Assets/Scripts/Tasks/Toilet/PlungerSpawner.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlungerSpawner : InteractableBehavior
+{
+    [SerializeField] GameObject PlungerPrefab;
+
+    private PlayerMovement player;
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>();
+    }
+
+
+
+    public override void Interact()
+    {
+        var spawned = Instantiate(PlungerPrefab, player.transform);
+        spawned.transform.SetParent(player.transform);
+    }
+
+    /*public override void AssignKey(string key)
+    {
+        Key = key;
+    }
+
+    public override void InRange(bool isInRange)
+    {
+        IsInRange = isInRange;
+    }*/
+}
